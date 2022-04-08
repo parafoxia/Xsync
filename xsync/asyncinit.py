5e5a33732098982492a7b66de6281739f6622079
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-04-08 11:31:37 +0100

    Add AsyncInitMixin class

--- xsync/asyncinit.py
+++ xsync/asyncinit.py
@@ -26,19 +26,43 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__all__ = ("AsyncInitMixin", "as_hybrid", "maybe_async", "set_async_impl")
+from __future__ import annotations
 
-__productname__ = "xsync"
-__version__ = "0.2.1"
-__description__ = "A set of tools to create hybrid sync/async interfaces."
-__url__ = "https://github.com/parafoxia/Xsync"
-__author__ = "Ethan Henderson"
-__author_email__ = "ethan.henderson.1998@gmail.com"
-__license__ = "BSD 3-Clause 'New' or 'Revised' License"
-__bugtracker__ = "https://github.com/parafoxia/Xsync/issues"
-__ci__ = "https://github.com/parafoxia/Xsync/actions"
-__changelog__ = "https://github.com/parafoxia/Xsync/releases"
+import inspect
+import logging
+import typing as t
 
-from .asyncinit import AsyncInitMixin
-from .deco import maybe_async
-from .hybrid import as_hybrid, set_async_impl
+_log = logging.getLogger(__name__)
+
+
+class AsyncInitMixin:
+    __async_init__: t.Callable[..., t.Any]
+
+    def __new__(
+        cls: type[AsyncInitMixin], *args: t.Any, **kwargs: t.Any
+    ) -> AsyncInitMixin:
+        obj = super(AsyncInitMixin, cls).__new__(cls)
+        orig_init = cls.__init__
+
+        def __init__(self: AsyncInitMixin, *args: t.Any, **kwargs: t.Any) -> None:
+            ctx = inspect.stack()[1].code_context
+
+            if not ctx or "await" not in ctx[0]:
+                _log.debug(f"Initialising {cls.__name__!r} normally")
+                return orig_init(obj, *args, **kwargs)
+
+            cls._args = args
+            cls._kwargs = kwargs
+
+        setattr(cls, "__init__", __init__)
+        return obj
+
+    def __await__(self) -> t.Generator[t.Any, t.Any, AsyncInitMixin]:
+        async def main() -> AsyncInitMixin:
+            _log.debug(f"Initialising {self.__class__.__name__!r} asynchronously")
+            await self.__async_init__(*self.__class__._args, *self.__class__._kwargs)
+            del self.__class__._args
+            del self.__class__._kwargs
+            return self
+
+        return main().__await__()
